c5f324bdbf96d0f68d26d27c8211beea11f7ea6d
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-04 17:32:46 +0530

    Update Backward feature selection.py

--- Backward feature selection.py
+++ Backward feature selection.py
@@ -24,15 +24,10 @@ from sklearn.model_selection import train_test_split
 y = df['Survived']
 x = df.drop('Survived',axis=1)
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
-
-
 from sklearn.ensemble import RandomForestClassifier 
 rf=RandomForestClassifier()
-
 from mlxtend.feature_selection import SequentialFeatureSelector 
-
 sfs=SequentialFeatureSelector(rf,k_features=10,forward=False,verbose=2,cv=5)
-
 sfs.fit(x_train,y_train)
 
 pd.DataFrame.from_dict(sfs.get_metric_dict()).T
