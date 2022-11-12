d389e34dec06d9c0a633b5ff2f876a5e7949890b
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-12 16:49:03 +0530

    Update and rename Exp-2(1),16-09-22.py to Linear Regression.py

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,117 +1,71 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
+#Name : Vishnu Kumar S.R
 
 
+#load the packages for loading the file
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[3]:
-
-
+#load the csv file using pandas as pd
 df=pd.read_csv("C:/Users/vknsr/Downloads/Iris.csv")
 df = df.set_index('Id')
 
+df.head() # get the top 5 values from the data set
 
-# In[4]:
+df.describe() #describe the data set and its data types
 
-
-df.head()
-
-
-# In[5]:
-
-
-df.describe()
-
-
-# In[7]:
-
-
+#split the data for training and testing
 x=df.iloc[:,1:2]
 y=df.iloc[:,4]
 
 
-# In[97]:
-
-
+#load the train_test_split package from sklearn
 from sklearn.model_selection import train_test_split 
 
 
-# In[98]:
-
-
+#fit the values of x,y
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
 
 
-# In[99]:
-
-
+#load the preprocessing package from sklearn and fit the model
 from sklearn.preprocessing import StandardScaler    
 st_x= StandardScaler()  
 x_train= st_x.fit_transform(x_train)    
 x_test= st_x.transform(x_test) 
 
 
-# In[100]:
-
-
+#from tree import decision tree classifier
 from sklearn.tree import DecisionTreeClassifier
-
-
-# In[101]:
 
 
 classifier= DecisionTreeClassifier(criterion='entropy', random_state=0)  
 
-
-# In[102]:
-
-
+#fit the training data in the model
 classifier.fit(x_train,y_train)
 
 
-# In[103]:
-
-
+#calculate the prediction value
 y_pred= classifier.predict(x_test) 
-
-
-# In[104]:
 
 
 y_pred
 
 
-# In[105]:
-
-
 from sklearn import tree
 
 
-# In[107]:
-
-
+#plot the decision tree
 print(tree.plot_tree(classifier,filled=True))
-
-
-# In[108]:
 
 
 from sklearn.metrics import accuracy_score
 
-
-# In[109]:
-
-
+#get the accuracy score using sklear.metrics
 accuracy_score(y_test,y_pred)
 
-
-# In[ ]:
-
+#Output:
+0.4
 
 
 
