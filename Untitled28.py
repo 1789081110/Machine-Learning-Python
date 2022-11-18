6e2c1338d23bee1c3621327ccefbe0e98cf93500
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-18 22:34:13 +0530

    Add files via upload

---
+++ Untitled28.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[211]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[236]:
+
+
+df_train=pd.read_csv("C:/Users/vknsr/Downloads/Dataset_Student_Academic_Success_Prediction/train.csv")
+df_test=pd.read_csv("C:/Users/vknsr/Downloads/Dataset_Student_Academic_Success_Prediction/test.csv")
+df_test.head()
+
+
+# In[237]:
+
+
+from xgboost import XGBClassifier
+xgb=XGBClassifier()
+
+
+# In[238]:
+
+
+get_ipython().system('pip install xgboost')
+
+
+# In[239]:
+
+
+df1=df_train.pop('Target')
+
+
+# In[240]:
+
+
+xg.fit(df_train_final, df1)
+
+
+# In[241]:
+
+
+y_pred=xg.predict(df_test)
+y_pred
+
+
+# In[182]:
+
+
+submission=pd.DataFrame({"Target":y_ptest})
+submission.to_csv('submission',index=False)
+
+
+# In[183]:
+
+
+pd.read_csv('submission.csv').mean()
+
+
+# In[ ]:
+
+
+
+
---
+++ Untitled35.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+# In[2]:
+
+
+df_train=pd.read_csv("C:/Users/vknsr/Downloads/Dataset_Student_Academic_Success_Prediction/train.csv",nrows=10000)
+df_test=pd.read_csv("C:/Users/vknsr/Downloads/Dataset_Student_Academic_Success_Prediction/test.csv")
+df_train.head()
+
+
+# In[3]:
+
+
+df_train.head()
+
+
+# In[4]:
+
+
+from sklearn.naive_bayes import GaussianNB
+
+
+# In[7]:
+
+
+ga=GaussianNB()
+df1=df_train.pop('Target')
+
+
+# In[9]:
+
+
+ga.fit(df_train,df1)
+
+
+# In[10]:
+
+
+y_pred=ga.predict(df_test)
+
+
+# In[12]:
+
+
+y_pred.mean()
+
+
+# In[ ]:
+
+
+
+
